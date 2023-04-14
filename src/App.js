53efc1ba97938967599216468cb7d7578dc7ae81
tweezerchef <ltomblock@gmail.com>
2023-04-14 17:11:23 -0500

    ?

--- src/App.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const App = () => (0, jsx_runtime_1.jsx)("h1", { children: "Hello from React!" });
-exports.default = App;
