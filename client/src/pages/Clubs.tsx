86559f5b3ad4225c8607e745e4eeb081c5558283
kevin <kklein166@gmail.com>
2023-04-17 15:40:07 -0500

    set up Clubs

--- client/src/pages/Clubs.tsx
+++ client/src/pages/Clubs.tsx
@@ -1,5 +1,18 @@
-function Clubs() {
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
+function Clubs() {
+  const [clubs, setClubs] = useState([]);
+
+  useEffect(() => {
+
+  })
+
+  return (
+    <div>
+      <h1>Book Clubs</h1>
+    </div>
+  )
 }
 
 export default Clubs;
--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ const path = require('path');
 const express = require('express');
 require('dotenv').config();
 
-
 const app = express();
 const CLIENT_PATH = path.resolve(__dirname, '../client/build');
 app.use(express.static(CLIENT_PATH));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// make sure this is the last route in our server
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./client/build', 'index.html'));
+});
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`Server listening on :${PORT}`);
-  });
+  console.log(`Server listening on :${PORT}`);
+});
