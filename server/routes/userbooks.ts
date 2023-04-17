88f9da5b594dba6931f8a90fcfb6643815fd31a5
matthewpmi <mpmekita.gmail.com>
2023-04-17 17:17:12 -0400

    ADD routes, userbooks.ts

--- server/routes/userbooks.ts
+++ server/routes/userbooks.ts
@@ -9,47 +9,38 @@ UserBooks.post('/', async (req, res) => {
   try {
     const { title, wishlist, owned } = req.body;
 
-    // Make Axios request to get book from API
+    // make request to get book from API
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=YOUR_API_KEY`);
     const bookData = response.data.items[0].volumeInfo;
 
-    // Add book to database
-    UserBooks.post('/', async (req, res) => {
-      try {
-        const { title, wishlist, owned } = req.body;
-
-        // Make Axios request to get book from API
-        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=YOUR_API_KEY`);
-        const bookData = response.data.items[0].volumeInfo;
-
-        // Add book to database
-        const createdBook = await prisma.books.create({
-          data: {
-            title: bookData.title,
-            author: bookData.authors[0],
-            description: bookData.description,
-            genre: { create: bookData.categories.map(name => ({ name })) },
-            paperback: bookData.printType === 'BOOK',
-            content: bookData.contentVersion,
+    // add book to database
+    const createdBook = await prisma.books.create({
+      data: {
+        title: bookData.title,
+        author: bookData.authors[0],
+        description: bookData.description,
+        genre: { create: bookData.categories.map(name => ({ name })) },
+        paperback: bookData.printType === 'BOOK',
+        content: bookData.contentVersion,
+        user: { connect: { id: req.user.id } },
+        UserBooks: {
+          create: {
+            wishlist,
+            owned,
             user: { connect: { id: req.user.id } },
-            UserBooks: {
-              create: {
-                wishlist,
-                owned,
-              },
-            },
           },
-          include: {
-            UserBooks: true,
-          },
-        });
-
-        res.json(createdBook);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Something went wrong' });
-      }
+        },
+      },
+      include: {
+        UserBooks: true,
+      },
     });
 
+    res.json(createdBook);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
 
 module.exports = UserBooks;
