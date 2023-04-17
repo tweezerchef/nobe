e7bcf64a182ac7a32d998987f2f7005f70b21ca8
neilthemippy <shapironeila@gmail.com>
2023-04-17 14:38:46 -0500

    pulled from upstream

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const CLIENT_PATH = path.resolve(__dirname, '../client/build');
 app.use(express.static(CLIENT_PATH));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//make sure this is the last route in our server
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./client/build', 'index.html'));
+});
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server listening on :${PORT}`);
