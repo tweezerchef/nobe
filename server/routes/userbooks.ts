b8ca5fa6c860d21fe001042bf8ae76f3bc5568dd
matthewpmi <mpmekita.gmail.com>
2023-04-17 16:50:24 -0400

    ADD routes folder

--- server/routes/userbooks.ts
+++ server/routes/userbooks.ts
@@ -1,9 +1,55 @@
 const express = require('express');
+const axios = require('axios')
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 const UserBooks = express.Router();
 
+UserBooks.post('/', async (req, res) => {
+  try {
+    const { title, wishlist, owned } = req.body;
+
+    // Make Axios request to get book from API
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=YOUR_API_KEY`);
+    const bookData = response.data.items[0].volumeInfo;
+
+    // Add book to database
+    UserBooks.post('/', async (req, res) => {
+      try {
+        const { title, wishlist, owned } = req.body;
+
+        // Make Axios request to get book from API
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=YOUR_API_KEY`);
+        const bookData = response.data.items[0].volumeInfo;
+
+        // Add book to database
+        const createdBook = await prisma.books.create({
+          data: {
+            title: bookData.title,
+            author: bookData.authors[0],
+            description: bookData.description,
+            genre: { create: bookData.categories.map(name => ({ name })) },
+            paperback: bookData.printType === 'BOOK',
+            content: bookData.contentVersion,
+            user: { connect: { id: req.user.id } },
+            UserBooks: {
+              create: {
+                wishlist,
+                owned,
+              },
+            },
+          },
+          include: {
+            UserBooks: true,
+          },
+        });
+
+        res.json(createdBook);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Something went wrong' });
+      }
+    });
 
 
 module.exports = UserBooks;
