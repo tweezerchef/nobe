348024b289d4f82c74472cac0d3b3d3fc2ad8946
kevin <kklein166@gmail.com>
2023-04-17 20:36:36 -0500

    change routes back for userbooks/clubs

--- server/routes/userbooks.ts
+++ server/routes/userbooks.ts
@@ -1,53 +1,47 @@
-// const express = require('express');
-// const axios = require('axios')
-// const { PrismaClient } = require('@prisma/client');
+const express = require('express');
+const axios = require('axios')
+const { PrismaClient } = require('@prisma/client');
 
-// const prisma = new PrismaClient();
-// const UserBooks = express.Router();
+const prisma = new PrismaClient();
+const UserBooks = express.Router();
 
+UserBooks.post('/:id', async (req, res) => {
+  try {
+    const { title, wishlist, owned } = req.body;
+    const { id } = req.params;
 
-const UserBooks = ({ express, axios, PrismaClient, prisma }) => {
-  const router = express.Router();
+    // make request to get book from API
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=`);
+    const bookData = response.data.items[0].volumeInfo;
 
-  router.post('/', async (req, res) => {
-    try {
-      const { title, wishlist, owned } = req.body;
-
-      // make request to get book from API
-      const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=YOUR_API_KEY`);
-      const bookData = response.data.items[0].volumeInfo;
-
-      // add book to database
-      const createdBook = await prisma.books.create({
-        data: {
-          title: bookData.title,
-          author: bookData.authors[0],
-          description: bookData.description,
-          genre: { create: bookData.categories.map(name => ({ name })) },
-          paperback: bookData.printType === 'BOOK',
-          content: bookData.contentVersion,
-          user: { connect: { id: req.user.id } },
-          UserBooks: {
-            create: {
-              wishlist,
-              owned,
-              user: { connect: { id: req.user.id } },
-            },
+    // add book to database
+    const createdBook = await prisma.books.create({
+      data: {
+        title: bookData.title,
+        author: bookData.authors[0],
+        description: bookData.description,
+        genre: { create: bookData.categories.map(name => ({ name })) },
+        paperback: bookData.printType === 'BOOK',
+        content: bookData.contentVersion,
+        user: { connect: { id: id } },
+        UserBooks: {
+          create: {
+            wishlist,
+            owned,
+            user: { connect: { id: id } },
           },
         },
-        include: {
-          UserBooks: true,
-        },
-      });
+      },
+      include: {
+        UserBooks: true,
+      },
+    });
 
-      res.json(createdBook);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Something went wrong' });
-    }
-  });
-
-  return router;
-}
+    res.json(createdBook);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
 
 module.exports = UserBooks;
