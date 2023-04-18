e07d3f2a090c9f8d4f5ca79a48cc2b48905ff3b0
neilthemippy <shapironeila@gmail.com>
2023-04-18 14:49:47 -0500

    has as input box rednering that will search a users current location w/auto complete

--- client/src/pages/Nearme.tsx
+++ client/src/pages/Nearme.tsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete'
 
 
 
 function Locations() {
 
+const [userLocation, setUserLocation] = useState('');
+
+const onPlaceSelect = (value: any) => {
+  console.log(value);
+}
+
+const onSuggectionChange = (value: any) => {
+  console.log(value);
+}
 
 
   return (
     <div>
       <h1>Near Me</h1>
-      {/* <ul>
-        {clubs.map((club) => (
-          <li key={club.id}>
-            <Link to={`/clubs/${club.id}`}>{club.name}</Link>
-          </li>
-        ))}
-      </ul> */}
+      <GeoapifyContext apiKey="6d182d93697140e88a9e75ab8d892bc5">
+      <GeoapifyGeocoderAutocomplete placeholder="Enter address here"
+        placeSelect={onPlaceSelect}
+        suggestionsChange={onSuggectionChange}
+        />
+    </GeoapifyContext>
+
     </div>
   )
 
