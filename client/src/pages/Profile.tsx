180b8444795332f10f56f6a38de0fd75198ab69c
tweezerchef <ltomblock@gmail.com>
2023-04-19 16:44:24 -0500

    bothDBWorking

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -35,7 +35,7 @@ const Profile = () => {
       {userBooks.map((book) => (
         <div key={book.id}>
           <h3>{book.books.title}</h3>
-          <p> <img src={book.books.image} /></p>
+          {/* <p> <img src={book.books.image} /></p> */}
           <p>{book.books.author}</p>
         </div>
       ))}
