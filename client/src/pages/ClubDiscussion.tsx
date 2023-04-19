366d3fc5117a388a00d0aae14f15170b738ca485
kevin <kklein166@gmail.com>
2023-04-19 17:17:56 -0500

    add state and useEffect with axios get request in ClubDiscussion

--- client/src/pages/ClubDiscussion.tsx
+++ client/src/pages/ClubDiscussion.tsx
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
-// interface ClubDiscussion {
-//   id: string;
-//   name: string;
-// }
+interface Discussion {
+  id: string;
+  name: string;
+}
 
 function ClubDiscussion() {
+  const { id } = useParams<{ id: string }>();
+  const [discussions, setDiscussion] = useState<Discussion | null>(null);
+
+  useEffect(() => {
+    async function fetchDiscussion() {
+      const response = await axios.get(`/api/clubs/${id}/discussion`);
+      setDiscussion(response.data);
+    }
+    fetchDiscussion();
+  }, [id])
+
 
   return (
     <div>
