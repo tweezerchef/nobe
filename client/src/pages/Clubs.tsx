13896c8ebf58d53789f07b42ebc09968e0c2d627
kevin <kklein166@gmail.com>
2023-04-20 11:42:14 -0500

    move create club form to Clubs page

--- client/src/pages/Clubs.tsx
+++ client/src/pages/Clubs.tsx
@@ -10,6 +10,8 @@ interface Club {
 
 function Clubs() {
   const [clubs, setClubs] = useState<Club[]>([]);
+  const [showForm, setShowForm] = useState(false);
+  const [newClubName, setNewClubName] = useState('');
 
   useEffect(() => {
     async function fetchClubs() {
@@ -20,12 +22,47 @@ function Clubs() {
     fetchClubs();
   }, []);
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setNewClubName(value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/api/create-club', { name: newClubName });
+      console.log(response.data);
+
+      setClubs([...clubs, response.data]); // add the new club to the state variable
+
+      setShowForm(false); // hide the form after submission
+      setNewClubName(''); // reset the input box
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>Book Clubs</h1>
-      <Button variant="contained" color="primary" component={Link} to="/create-club">
+      <Button variant="contained" color="primary" onClick={() => setShowForm(true)}>
         Create a new club
       </Button>
+
+      {showForm && (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={newClubName}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Create Club</button>
+        </form>
+      )}
+
       <ul>
         {clubs.map((club) => (
           <li key={club.id}>
