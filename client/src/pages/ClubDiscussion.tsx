648ab78d2981ef42d5226ddf23216818e344a6b0
kevin <kklein166@gmail.com>
2023-04-21 16:10:13 -0500

    add start new discussion button to ClubDiscussion, not updating db

--- client/src/pages/ClubDiscussion.tsx
+++ client/src/pages/ClubDiscussion.tsx
@@ -22,7 +22,10 @@ function ClubDiscussion() {
   const [hasJoined, setHasJoined] = useState(false);
   const searchParams = new URLSearchParams(location.search);
   const clubName = searchParams.get('name') || 'Book Club Discussion';
-  console.log(discussions);
+  const [newDiscussionTitle, setNewDiscussionTitle] = useState('');
+  const [newDiscussionBody, setNewDiscussionBody] = useState('');
+  const [showForm, setShowForm] = useState(false);
+
 
   useEffect(() => {
     async function fetchDiscussion() {
@@ -54,6 +57,23 @@ function ClubDiscussion() {
     }
   };
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(`/api/clubs/${id}/discussion`, {
+        title: newDiscussionTitle,
+        body: newDiscussionBody,
+      });
+      setDiscussions([...discussions, response.data]);
+      setNewDiscussionTitle('');
+      setNewDiscussionBody('');
+      setShowForm(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>{clubName}</h1>
@@ -65,6 +85,26 @@ function ClubDiscussion() {
       >
         {hasJoined ? "Joined" : "Join"}
       </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setShowForm(!showForm)}
+      >
+        Start new discussion
+      </Button>
+      {showForm && (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={newDiscussionTitle}
+            onChange={(event) => setNewDiscussionTitle(event.target.value)}
+          />
+          <button type="submit">Create New Discussion</button>
+        </form>
+      )}
       {discussions?.map((discussion) => (
         // console.log(discussion),
         <div key={discussion.id}>
