6c8a72e9fca75bf16e02ccfc4ac7493ed93fa54c
neilthemippy <shapironeila@gmail.com>
2023-04-22 12:06:54 -0500

    95% complete feature is working 100% need to figure out small other things like a put request to save user location and styling

--- client/src/pages/Nearme.tsx
+++ client/src/pages/Nearme.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete'
-import BookDisplay from "../components/BookDisplay/BookDisplay";
+import BookDisplay from "../components/MattsBookDisplay/BookDisplay";
+import Navbar from "../components/Navbar/Navbar";
 
 interface Book {
   books: {
@@ -39,14 +40,21 @@ const  getBooksNearMe = async () => {
     const res = await axios.get('/location/locations', { params: {lon: longitude, lat: latitude, radius: radius } });
     console.log(res);
     setBooksNearBy(res.data.userBooks);
-    const ownedBooks = booksNearBy.filter(book => book.owned).map((book) => book.books);
-    setDisplayBooks(ownedBooks);
   } catch (err) {
     console.error(err);
   }
 }
 
-console.log(displayBooks);
+console.log(booksNearBy, 'booksNeaBy')
+
+useEffect(() => {
+  const ownedBooks = booksNearBy.flat().filter(book => book.owned === true).map((book) => book.books);
+  console.log(ownedBooks, '69');
+  setDisplayBooks(ownedBooks);
+}, [booksNearBy]);
+
+console.log(displayBooks, 'displaybooks');
+
 
 const onPlaceSelect = (value: any) => {
   console.log(value);
@@ -54,9 +62,9 @@ const onPlaceSelect = (value: any) => {
   setLongitude(value.properties.lat);
 }
 
-console.log(longitude, '1');
-console.log(latitude, '2');
-console.log(radius, '3');
+// console.log(longitude, '1');
+// console.log(latitude, '2');
+// console.log(radius, '3');
 
 
   const onSuggectionChange = (value: any) => {
@@ -69,10 +77,12 @@ console.log(radius, '3');
   };
 
 ;
-//console.log(ownedBooks, '69');
+
+
 
 return (
    <div>
+    <Navbar children={undefined} />
       <h1>Near Me</h1>
       <GeoapifyContext apiKey="6d182d93697140e88a9e75ab8d892bc5">
         <GeoapifyGeocoderAutocomplete
