cfdcbc18fe89f15398f562cea4e41257572ae9ab
kevin <kklein166@gmail.com>
2023-04-26 12:29:10 -0500

    google places now working, loading suggestions, panning to selected address/location

--- client/src/components/ReadingSpotsMap/places.tsx
+++ client/src/components/ReadingSpotsMap/places.tsx
@@ -1,16 +1,47 @@
 import usePlacesAutoComplete, { getGeocode, getLatLng } from "use-places-autocomplete";
 import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox";
 import "@reach/combobox/styles.css";
+import "../../styles/mapstyles.css";
 
 type PlacesProps = {
   setOffice: (position: google.maps.LatLngLiteral) => void;
 };
 
 function Places({ setOffice }: PlacesProps) {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutoComplete();
+
+  // console.log({ status, data });
+
+  const handleSelect = async (val: string) => {
+    setValue(val, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address: val });
+    const { lat, lng } = await getLatLng(results[0]);
+    setOffice({ lat, lng });
+  }
+
   return (
-    <div>
-      Places
-    </div>
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={e => setValue(e.target.value)} className="combobox-input"
+        placeholder="Search an address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" && data.map(({ place_id, description }) => (
+            <ComboboxOption key={place_id} value={description} />
+          ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
   )
 }
 
