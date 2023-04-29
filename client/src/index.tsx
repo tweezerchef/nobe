f852676a285e5b367300f352c6da955e8c7d4a75
tweezerchef <ltomblock@gmail.com>
2023-04-29 14:06:15 -0500

    fuckingDarkModeDone

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,30 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";;
 import AppWrapper from './AppWrapper';
 import { StyledEngineProvider } from '@mui/material/styles';
-// import {
-//   Experimental_CssVarsProvider as MaterialCssVarsProvider,
-//   THEME_ID,
-// } from '@mui/material/styles';
-// import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy/styles';
-// import { experimental_extendTheme as materialExtendTheme } from '@mui/material/styles';
-// const materialTheme = materialExtendTheme({
-//   colorSchemes: {
-//     light: {
-//       palette: {
-//         primary: {
-//           main: pink[600],
-//         },
-//       },
-//     },
-//     dark: {
-//       palette: {
-//         primary: {
-//           main: pink[400],
-//         },
-//       },
-//     },
-//   },
-// });
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
