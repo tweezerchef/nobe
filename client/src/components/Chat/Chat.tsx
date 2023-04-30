1f0c79453114111c113d35e8eb97abd069d68948
tweezerchef <ltomblock@gmail.com>
2023-04-30 18:52:50 -0500

    goToProfileFromBigBookandChatAtTopOfScreen

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -27,9 +27,9 @@ const useStyles = makeStyles({
     width: '800px',
     height: '60vh'
   },
-  headBG: {
-    backgroundColor: '#e0e0e0'
-  },
+  // headBG: {
+  //   backgroundColor: '#e0e0e0'
+  // },
   borderRight500: {
     borderRight: '1px solid #e0e0e0'
   },
