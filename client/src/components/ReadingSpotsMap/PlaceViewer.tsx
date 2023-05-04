c27fa3c7ed1ac6073ed5b842d4aae87f71e32429
tweezerchef <ltomblock@gmail.com>
2023-05-04 13:50:39 -0500

    redesign

--- client/src/components/ReadingSpotsMap/PlaceViewer.tsx
+++ client/src/components/ReadingSpotsMap/PlaceViewer.tsx
@@ -1,13 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip,
-} from '@material-ui/core';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import PhoneIcon from '@material-ui/icons/Phone';
-import { Theme } from '@mui/material/styles';
-import Rating from '@mui/material/Rating';
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Box,
+  Rating,
+  Chip,
+  CardActions,
+  Button,
+  Grid,
+} from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
 import axios from 'axios';
-import useStyles from './styles';
 
 interface Award {
   images: {
@@ -41,18 +47,34 @@ interface Place {
   website: string;
 }
 
-interface PlaceDetailsProps {
-  place: Place;
-  selected: string | null;
-  refProp: React.MutableRefObject<HTMLElement | null>;
-}
+// interface PlaceDetailsProps {
+//   place: Place;
+//   selected: string | null;
+//   refProp: React.MutableRefObject<HTMLElement | null>;
+// }
+// // const useStyles = makeStyles((theme) => ({
+//   card: {
+//     height: '100%',
+//   },
+//   chip: {
+//     marginRight: theme.spacing(1),
+//   },
+//   subtitle: {
+//     display: 'flex',
+//     alignItems: 'center',
+//   },
+//   space: {
+//     display: 'flex',
+//     alignItems: 'center',
+//   },
+// }));
 
 // const PlaceDetails: React.FC<PlaceDetailsProps> = () => {
 // if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' })
 // { place, selected, refProp }
 function PlaceDetails() {
   const [place, setPlace] = useState<Place | null>(null);
-  const classes = useStyles();
+  //   const classes = useStyles();
 
   useEffect(() => {
     // const response = axios.get(`https://maps.googleapis.com/maps/api/place/details/json
@@ -60,71 +82,94 @@ function PlaceDetails() {
     // &fields=address_components
     // &key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`).then((response) => { console.log(response) })
     // console.log(response);
-    const response = axios.get('/api/places-to-read/getplace').then((response) => { console.log(response), setPlace(response.data.result); });
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    axios.get('/api/places-to-read/getplace').then((response) => { console.log(response), setPlace(response.data.result); });
   }, []);
   return (
 
-    <Card elevation={6}>
-      <CardMedia
-        style={{ height: 350 }}
-        image={place?.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
-        title={place?.name}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5">{place?.name}</Typography>
-        <Box display="flex" justifyContent="space-between" my={2}>
-          <Rating name="read-only" value={Number(place?.rating)} readOnly />
-          <Typography component="legend">
-            {place?.num_reviews}
-            {' '}
-            review
-            {place?.num_reviews !== 1 && 's'}
-          </Typography>
-        </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography component="legend">Price</Typography>
-          <Typography gutterBottom variant="subtitle1">
-            {place?.price_level}
-          </Typography>
-        </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography component="legend">Ranking</Typography>
-          <Typography gutterBottom variant="subtitle1">
-            {place?.ranking}
-          </Typography>
-        </Box>
-        {place?.awards?.map((award) => (
-          <Box display="flex" justifyContent="space-between" my={1} alignItems="center">
-            <img src={award.images.small} />
-            <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
-          </Box>
-        ))}
-        {place?.cuisine?.map(({ name }) => (
-          <Chip key={name} size="small" label={name} className={classes.chip} />
-        ))}
-        {place?.address && (
-        <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
-          <LocationOnIcon />
-          {place.address}
-        </Typography>
-        )}
-        {place?.phone && (
-        <Typography variant="body2" color="textSecondary" className={classes.spacing}>
-          <PhoneIcon />
-          {' '}
-          {place.phone}
-        </Typography>
-        )}
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="primary" onClick={() => window.open(place?.web_url, '_blank')}>
-          Trip Advisor
-        </Button>
-        <Button size="small" color="primary" onClick={() => window.open(place?.website, '_blank')}>
-          Website
-        </Button>
-      </CardActions>
-    </Card>
+    <Grid container sx={{ height: '100%' }}>
+      <Grid item xs={12}>
+        <Card
+          elevation={6}
+          sx={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Grid container>
+            <Grid item xs={12} sm={6}>
+              <CardMedia
+                sx={{ paddingTop: '100%', width: '100%' }} // Make the image a square
+                image={
+                place?.photo
+                  ? place.photo.images.large.url
+                  : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/restaurant-placeholder-001.jpg'
+              }
+                title={place?.name}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <CardContent>
+                <Typography gutterBottom variant="h5">{place?.name}</Typography>
+                <Box display="flex" justifyContent="space-between" my={2}>
+                  <Rating name="read-only" value={Number(place?.rating)} readOnly />
+                  <Typography component="legend">
+                    {place?.num_reviews}
+                    {' '}
+                    review
+                    {place?.num_reviews !== 1 && 's'}
+                  </Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                  <Typography component="legend">Price</Typography>
+                  <Typography gutterBottom variant="subtitle1">
+                    {place?.price_level}
+                  </Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                  <Typography component="legend">Ranking</Typography>
+                  <Typography gutterBottom variant="subtitle1">
+                    {place?.ranking}
+                  </Typography>
+                </Box>
+                {place?.awards?.map((award) => (
+                  <Box display="flex" justifyContent="space-between" my={1} alignItems="center">
+                    <img src={award.images.small} />
+                    <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+                  </Box>
+                ))}
+                {place?.cuisine?.map(({ name }) => (
+                  <Chip key={name} size="small" label={name} sx={{ marginRight: 1 }} />
+                ))}
+                {place?.address && (
+                <Typography gutterBottom variant="body2" color="text.secondary" sx={{ display: 'flex', alignItems: 'center' }}>
+                  <LocationOnIcon />
+                  {place?.address}
+                </Typography>
+
+                )}
+                {place?.phone && (
+                <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', alignItems: 'center' }}>
+                  <PhoneIcon />
+                  {place?.phone}
+                </Typography>
+                )}
+              </CardContent>
+            </Grid>
+          </Grid>
+          <CardActions>
+            <Button size="small" color="primary" onClick={() => window.open(place?.web_url, '_blank')}>
+              Trip Advisor
+            </Button>
+            <Button size="small" color="primary" onClick={() => window.open(place?.website, '_blank')}>
+              Website
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
