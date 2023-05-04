c5d9bd7e08bd219acad1dc17cd39142848430331
tweezerchef <ltomblock@gmail.com>
2023-05-04 09:41:23 -0500

    WensMorn

--- server/routes/spotsmap.ts
+++ server/routes/spotsmap.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { SidebarBody } from '../../client/src/Styled';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -9,7 +11,7 @@ const SpotsMapRoute = express.Router();
 
 
 SpotsMapRoute.post("/place", async (req: Request, res: Response) => {
-  const { address, lat, lng } = req.body;
+  const { address, lat, lng, altLoc } = req.body;
   try {
     const existingPlace = await prisma.placesToRead.findFirst({
       where: {
@@ -28,30 +30,88 @@ SpotsMapRoute.post("/place", async (req: Request, res: Response) => {
           Location: address,
           Lat: lat,
           Long: lng,
+          altLoc: altLoc,
         },
       });
       res.status(201).json({ createdPlace });
     }
+    //   const createdPlace = await prisma.placesToRead.upsert({
+    //     where: { Lat: lat, Long: lng },
+    // })
   } catch (error) {
     console.error(error);
     res.status(500).send("Something went wrong");
   }
 });
+SpotsMapRoute.get('/getplace', async (req: Request, res: Response) => {
+  const { placeId } = req.query
+  console.log('poop')
+  try {
+    const place = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}=${process.env.GOOGLE_MAPS_API_KEY}`)
+
+    res.send(place.data)
+  }
+  catch (error) {
+    console.error(error);
+  }
+})
 
 SpotsMapRoute.get('/', async (req: Request, res: Response) => {
-  const places = await prisma.placesToRead.findMany();
-  res.json(places);
+  try {
+    const places = await prisma.placesToRead.findMany({
+      select: {
+        id: true,
+        nickName: true,
+        Location: true,
+        Private: true,
+        Lat: true,
+        Long: true,
+        altLoc: true,
+        LendingTableIn: true,
+        LendingTableOut: true,
+        userPlaces: {
+          select: {
+            id: true,
+            userId: true,
+            placeId: true,
+            Rating: true,
+            Review: true,
+            CheckIns: true,
+            place: true,
+            user: true,
+          }
+        },
+        Activity: true,
+        Description_Places: {
+          select: {
+            id: true,
+            body: true,
+            user: true,
+          }
+        }
+      }
+    });
+
+    //console.log(places)
+    res.send(places);
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
 });
 
 SpotsMapRoute.post('/description', async (req: Request, res: Response) => {
-  const { Description, placeId } = req.body;
+  const { body, placeId, userId } = req.body;
 
 
   try {
     const updatedPlace = await prisma.description_Places.upsert({
-      where: { id: placeId },
-      data: { Description: Description },
+      where: { userId_placeId: { placeId: placeId, userId: userId } },
+      update: { body },
+      create: { body, placeId, userId, },
     });
+    //console.log(updatedPlace)
 
     res.status(200).json({ updatedPlace });
   } catch (error) {
