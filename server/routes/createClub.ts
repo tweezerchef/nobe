63013427b5f82818cfd5f23fa59e9976c1d418c7
kevin <kklein166@gmail.com>
2023-05-05 12:03:15 -0500

    clean up

--- server/routes/createClub.ts
+++ server/routes/createClub.ts
@@ -8,24 +8,23 @@ async function findOrCreateClub(
   name: string,
   description: string,
   image: string,
-  createdBy: string, // new parameter
+  createdBy: string,
 ) {
   const newClub = await prisma.clubs.upsert({
     where: { name },
     update: {},
     create: {
       name, description, image, clubMembers: { create: { userId: createdBy } },
-    }, // create clubMembers relationship
-    include: { clubMembers: true }, // include clubMembers in the response
+    },
+    include: { clubMembers: true },
   });
   return newClub;
 }
 
 CreateClubRoute.post('/', async (req: Request, res: Response) => {
-  const createdBy = req.body.userId; // get the userId from the request body
+  const createdBy = req.body.userId;
 
   findOrCreateClub(req.body.name, req.body.description, req.body.image, createdBy)
-    .then((newClub) => console.log(newClub))
     .then(async () => {
       const clubs = await prisma.clubs.findMany();
       res.json(clubs);
