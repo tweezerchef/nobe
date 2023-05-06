23a9629a4218cf638fb7a204d4cbb04494fdf46e
kevin <kklein166@gmail.com>
2023-05-06 13:40:22 -0500

    move styling and fix merge errors

--- client/src/pages/ClubDiscussion.tsx
+++ client/src/pages/ClubDiscussion.tsx
@@ -1,35 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 import {
-  Button, Card, CardContent, Typography, TextField, FormControl, Box,
+  Button, TextField, FormControl,
 } from '@material-ui/core';
 import axios from 'axios';
 import { ClubHeader } from './style';
 import JoinClubButton from '../components/Button/JoinClubButton';
 import '../styles/clubDiscussionStyle.css';
+import DiscussionList from '../components/DiscussionForum/Discussions';
 
-interface DiscussionPost {
-  id: string;
-  body: string;
-  userId: string;
-  discussionId: string;
-}
+// interface DiscussionPost {
+//   id: string;
+//   body: string;
+//   userId: string;
+//   discussionId: string;
+// }
 
-interface Discussion {
-  id: string;
-  Posts: DiscussionPost[];
-  title: string;
-}
+// interface Discussion {
+//   id: string;
+//   Posts: DiscussionPost[];
+//   title: string;
+// }
 
 function ClubDiscussion() {
   const { id } = useParams<{ id: string }>();
   const [discussions, setDiscussions] = useState<Discussion[]>([]);
-  const [hasJoined, setHasJoined] = useState(false);
-  const searchParams = new URLSearchParams(location.search);
-  const clubName = searchParams.get('name') || 'Book Club Discussion';
   const [newDiscussionTitle, setNewDiscussionTitle] = useState('');
   const [showForm, setShowForm] = useState(false);
+
+  const searchParams = new URLSearchParams(location.search);
+  const clubName = searchParams.get('name');
+  const clubId = id;
+  // const userContext = useContext(UserContext);
+  // const user = userContext?.user;
+  // console.log(user);
 
   useEffect(() => {
     async function fetchDiscussion() {
@@ -43,66 +48,40 @@ function ClubDiscussion() {
     if (id) {
       fetchDiscussion();
     }
-  }, [id]);
-
-  const handleJoinClub = async () => {
-    try {
-      const user = localStorage.getItem('user');
-      if (!user) {
-        throw new Error('No user found');
-      }
-      const parsed = JSON.parse(user);
-      const { email } = parsed;
-
-      const { data: club } = await axios.get(`/api/clubs/${id}`);
-      const memberEmails = club.members?.map((member: { email: string; }) => member.email);
-
-      if (memberEmails && memberEmails.includes(email)) {
-        setHasJoined(true);
-        return;
-      }
-      await axios.post(`/api/clubs/${id}/join`, { email });
-      setHasJoined(true);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  }, []);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     try {
       const user = localStorage.getItem('user');
 
       if (!user) {
         throw new Error('No user found');
+      }
+      if (!newDiscussionTitle) {
+        alert('Please enter a discussion title');
+        return;
       }
       const parsed = JSON.parse(user);
       const response = await axios.post(`/api/clubs/${id}/discussion`, {
         title: newDiscussionTitle,
         userId: parsed.id,
       });
-      setDiscussions([...discussions, response.data]);
+      setDiscussions((discussions) => [...discussions, response.data]);
       setNewDiscussionTitle('');
       setShowForm(false);
     } catch (error) {
       console.error(error);
-    }
+    } finally { console.log(discussions); }
   };
 
   return (
     <div>
       <ClubHeader style={{ textAlign: 'center' }}>{clubName}</ClubHeader>
+
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Stack spacing={2} direction="row">
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={hasJoined}
-            onClick={handleJoinClub}
-          >
-            {hasJoined ? 'Leave' : 'Join'}
-          </Button>
+          <JoinClubButton clubId={clubId} />
           <Button
             variant="contained"
             color="primary"
@@ -126,7 +105,8 @@ function ClubDiscussion() {
           </FormControl>
         </form>
       )}
-      {discussions?.map((discussion) => (
+      {id && <DiscussionList discussions={discussions} clubId={id} key={discussions.length} />}
+      {/* {discussions?.map((discussion) => (
         <Box sx={{ my: 1 }}>
           <Card key={discussion.id} className="forum-card">
             <Link
@@ -146,7 +126,7 @@ function ClubDiscussion() {
             </Link>
           </Card>
         </Box>
-      ))}
+      ))} */}
     </div>
   );
 }
