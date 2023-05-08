d541211cbbfa956fc3893090b61a5a38aeaa4e39
neilthemippy <shapironeila@gmail.com>
2023-05-07 20:00:02 -0500

    pulling from upstream working on profile cards

--- client/src/components/UserDisplay/Userdisplay.tsx
+++ client/src/components/UserDisplay/Userdisplay.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-lone-blocks */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {
-// useContext,
+  useContext,
   useEffect, useState,
 } from 'react';
 import Avatar from '@mui/material/Avatar';
@@ -8,7 +9,19 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 // import Book from '../Book/Book';
 // import UserContext from '../../hooks/Context';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { Container } from '@material-ui/core';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import styled from 'styled-components';
 import BookDisplay from '../BookDisplay/BookDisplay';
+import {
+  ProfileCard, MessageButton, FollowButton, Action, StatusText, StatusValue, Status,
+  Name, Desc, ProfileInfo, ProfileImage,
+} from './style';
 
 interface UserProfile {
   id: string;
@@ -26,27 +39,6 @@ function UserDisplay({ user }: { user: any }) {
 
   const [books, setBooks] = useState<any>([]);
 
-  // const getUserBooks = () => {
-  // const booksArray = userBooks?.map((userbook: any) => userbook.Books);
-  // console.log(booksArray, 20);
-  // setBooks(booksArray);
-  // }
-
-  // useEffect(() => {
-  //   if (profile && user.UserBooks) {
-  //     getUserBooks();
-  //   }
-  // }, []);
-  // const getProfile = async () => {
-  //   try {
-  //     const response = await axios.get(`/user/id?id=${user.id}`);
-  //     // console.log(response.data)
-  //     setProfile(response.data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
   useEffect(() => {
     const booksArray = userBooks?.map((userbook: any) => userbook.Books);
     // console.log(booksArray, 20);
@@ -57,12 +49,55 @@ function UserDisplay({ user }: { user: any }) {
 
   return (
     <div>
-      <Link to={`/profile/${user.id}`}>
-        <div className="user-firstName">{user.firstName}</div>
-        <Avatar alt="user-image" src={user.picture} />
-      </Link>
+      <ProfileCard>
+        <Avatar
+          src={user.picture}
+          alt={user.name}
+          style={{ width: '7rem', height: '7rem' }}
+        />
+        <ProfileInfo>
+          <Name variant="h2">{user.firstName}</Name>
+          <Desc>{user.description}</Desc>
+          <Status>
+            <li>
+              <StatusValue variant="h3" />
+              <StatusText>Posts</StatusText>
+            </li>
+            <li>
+              <StatusValue variant="h3" />
+              <StatusText>Followers</StatusText>
+            </li>
+            <li>
+              <StatusValue variant="h3">{user.following}</StatusValue>
+              <StatusText>Following</StatusText>
+            </li>
+          </Status>
+          <Action>
+            <FollowButton variant="contained">Follow</FollowButton>
+            <MessageButton variant="contained">Message</MessageButton>
+          </Action>
+        </ProfileInfo>
+      </ProfileCard>
       <BookDisplay books={books} />
     </div>
   );
 }
+
+{ /* <Link to={`/profile/${user.id}`}>
+        <div className="user-firstName" />
+        <StyledCard>
+          <ProfileImage>
+            <ProfilePicture src={user.picture} />
+          </ProfileImage>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {user.firstName}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <PinkButton> Share </PinkButton>
+            <GrayOutlineButton> Learn More </GrayOutlineButton>
+          </CardActions>
+        </StyledCard>
+      </Link> */ }
 export default UserDisplay;
--- client/src/components/UserDisplay/style.ts
+++ client/src/components/UserDisplay/style.ts
@@ -1,89 +1,85 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import styled from 'styled-components';
 
-const ProfileCard = styled.div`
-  width: 300px;
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 2rem;
-  background-color: white;
-  border-radius: 1rem;
-  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.6);
-`;
+const ProfileCard = styled(Card)({
+  display: 'flex',
+  width: 450,
+  height: 150,
+  margin: '0 auto',
+  boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.6)',
+});
 
-const ProfileImage = styled.div`
-  position: relative;
-  top: -60px;
-  border-radius: 1rem;
-  background-image: url(https://yt3.ggpht.com/ytc/AKedOLSB-oR-xmvVSZXJ3gbK12uvv0AJUvajwxMie_R_uw=s900-c-k-c0x00ffffff-no-rj);
-  width: 11rem;
-  height: 11rem;
-  background-position: center;
-  background-size: cover;
-  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.4);
-`;
+const ProfileImage = styled(CardMedia)({
+  width: 200,
+  height: 200,
+});
 
-const ProfileInfo = styled.div`
-  text-align: center;
-  margin-top: -3rem;
-  margin-bottom: 1rem;
-`;
+const ProfileInfo = styled(CardContent)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  textAlign: 'center',
+});
 
-const ProfileName = styled.h3`
-  color: #212121;
-`;
+const Name = styled(Typography)({
+  color: '#212121',
+  fontWeight: 'bold',
+  fontSize: '24px',
+});
 
-const ProfileDesc = styled.p`
-  color: #666666;
-  font-size: 0.9rem;
-`;
+const Desc = styled(Typography)({
+  color: '#666666',
+  fontSize: '18px',
+});
 
-const Status = styled.ul`
-  list-style: none;
-  display: flex;
-  justify-content: space-between;
-  text-align: center;
-  line-height: 1rem;
-  margin-bottom: 1.3rem;
-`;
+const Status = styled('ul')({
+  display: 'flex',
+  justifyContent: 'space-between',
+  listStyle: 'none',
+  textAlign: 'center',
+  lineHeight: '1rem',
+  marginBottom: '1.3rem',
+});
 
-const StatusValue = styled.span`
-  color: #212121;
-  font-weight: 700;
-`;
+const StatusValue = styled(Typography)({
+  color: '#212121',
+  fontWeight: 'bold',
+});
 
-const StatusText = styled.span`
-  font-size: 0.8rem;
-  color: #7c7c7d;
-`;
+const StatusText = styled(Typography)({
+  fontSize: '14px',
+  color: '#7c7c7d',
+});
 
-const Action = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
+const Action = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
 
-const PinkButton = styled.button`
-  border: none;
-  padding: 0.8em 1.9em;
-  border-radius: 0.35rem;
-  cursor: pointer;
-  font-weight: 600;
-  background: #b82151;
-  color: white;
-`;
+const FollowButton = styled(Button)({
+  backgroundColor: '#b82151',
+  color: 'white',
+  borderRadius: '0.35rem',
+  fontWeight: 'bold',
+  padding: '0.8em 1.9em',
+  cursor: 'pointer',
+  marginRight: '10px',
+});
 
-const GrayOutlineButton = styled.button`
-  border: 1px solid;
-  background: transparent;
-  color: #b82151;
-  border-radius: 0.35rem;
-  padding: 0.8em 1.9em;
-  cursor: pointer;
-  font-weight: 600;
-`;
+const MessageButton = styled(Button)({
+  backgroundColor: 'transparent',
+  color: '#b82151',
+  borderRadius: '0.35rem',
+  fontWeight: 'bold',
+  padding: '0.8em 1.9em',
+  cursor: 'pointer',
+});
 
 export {
-  GrayOutlineButton, PinkButton, Action,
-  ProfileImage, StatusValue, StatusText, Status, ProfileInfo, ProfileName, ProfileCard, ProfileDesc,
+  ProfileCard, MessageButton, FollowButton, Action, StatusText, StatusValue, Status,
+  Name, Desc, ProfileInfo, ProfileImage,
 };
