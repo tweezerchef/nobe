ffffa8af771d00e958703a612f07682495f63ac7
kevin <kklein166@gmail.com>
2023-05-25 16:55:50 -0500

    BookSearchButton work

--- client/src/components/Button/BookSearchButton.tsx
+++ client/src/components/Button/BookSearchButton.tsx
@@ -13,6 +13,7 @@ function BookSearchButton(props: any) {
   const [book, setBooks] = useState<any | null>(null);
   const [title, setTitle] = useState<string>('');
   const [open, setOpen] = React.useState(false);
+  const [timeline, setTimeLine] = useState<string>('');
   const { isDiscussionCreator } = props;
 
   const userContext = useContext(UserContext);
@@ -31,6 +32,7 @@ function BookSearchButton(props: any) {
     event.preventDefault();
     axios.get(`/bookdata/title/searchOne?title=${title}`).then((response) => { setBooks(response.data); });
   };
+
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
   };
@@ -44,6 +46,46 @@ function BookSearchButton(props: any) {
       )}
       <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
         <DialogTitle>Add a Book</DialogTitle>
+        <Box
+          component="form"
+          sx={{
+            margin: '20px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            '& > :not(style)': { m: 1 },
+            justifyContent: 'center',
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <form onSubmit={handleSubmit}>
+            <TextField
+              autoFocus
+              id="club-name"
+              label="Book Title"
+              variant="outlined"
+              type="string"
+              fullWidth
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+            <TextField
+              id="book-timeline"
+              label="Book Timeline for Readers"
+              variant="outlined"
+              type="string"
+              fullWidth
+              value={timeline}
+              onChange={(e) => setTimeLine(e.target.value)}
+              required
+            />
+            <Button variant="contained" color="primary" type="submit">
+              Create Club
+            </Button>
+          </form>
+        </Box>
       </Dialog>
     </div>
   );
