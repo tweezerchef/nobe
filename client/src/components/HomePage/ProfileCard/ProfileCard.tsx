d7ff8609bb19ecd4831ea5d913a557617602feef
tweezerchef <ltomblock@gmail.com>
2023-05-25 12:30:44 -0500

    switchToProfileBranch

--- client/src/components/HomePage/ProfileCard/ProfileCard.tsx
+++ client/src/components/HomePage/ProfileCard/ProfileCard.tsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Unstable_Grid2';
 import UserContext from '../../../hooks/Context';
 
 function ProfileCard() {
@@ -14,6 +15,7 @@ function ProfileCard() {
   const user = userContext?.user;
   const owned = 10;
   const wishlist = 10;
+
   return (
 
     <Box
@@ -40,7 +42,17 @@ function ProfileCard() {
         </Link>
       </Stack>
       <Divider sx={{ borderBottomWidth: 6, bgcolor: 'ThreeDDarkShadow' }} variant="middle" />
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
 
+        <Grid xs={4} sm={4} md={4}>
+          <Typography variant="body2" align="center">
+            Friends:
+            {' '}
+            {user?.friends.length}
+          </Typography>
+        </Grid>
+
+      </Grid>
     </Box>
   );
 }
