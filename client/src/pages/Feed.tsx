74c2fe262789ad9c3c56a44be0bc59314a163fe8
Michael Gandolfi <michaelgandolfi7@gmail.com>
2023-05-27 17:22:36 -0500

    pfp now clickabe and resized book display

--- client/src/pages/Feed.tsx
+++ client/src/pages/Feed.tsx
@@ -6,28 +6,9 @@ import {
 import axios from 'axios';
 import moment from 'moment';
 
+import { useNavigate } from 'react-router-dom';
 import Divider from '@mui/material/Divider';
 import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
-import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
-import StarIcon from '@mui/icons-material/Star';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import IconButton from '@mui/joy/IconButton';
-import LocalMallIcon from '@mui/icons-material/LocalMall';
-import AddLocationIcon from '@mui/icons-material/AddLocation';
 import UserContext from '../hooks/Context';
 
 interface Activity {
@@ -49,6 +30,8 @@ interface Activity {
 
 export default function CustomizedTimeline() {
   const [activity, setActivity] = useState<Activity[]>([]);
+
+  const navigate = useNavigate();
 
   const userContext = useContext(UserContext);
   const user = userContext?.user;
@@ -86,10 +69,10 @@ export default function CustomizedTimeline() {
           <div className="container">
             <div style={{ display: 'flex' }}>
               <div style={{ margin: '3%' }}>
-                <Avatar src={data.user?.picture} />
+                <Avatar onClick={() => navigate(`/profile/${data.user?.id}`)} src={data.user?.picture} />
               </div>
               <div style={{ marginTop: '3%', marginRight: '3%' }}>
-                <span style={{ fontWeight: 'bold' }}>
+                <span style={{ fontWeight: 'bold' }} onClick={() => navigate(`/profile/${data.user?.id}`)}>
                   {`${data.user.firstName}`}
                 </span>
                 <span style={{ marginLeft: '3%', color: 'grey' }}>
@@ -108,13 +91,12 @@ export default function CustomizedTimeline() {
                         )}
                   <div>
                     {data.type.toLowerCase() === 'wishlist' || data.type.toLowerCase() === 'owned' || data.type.toLowerCase() === 'review' ? (
-                      <img src={`${data.book.image}`} />
+                      <img style={{ width: "200px" }} src={`${data.book.image}`} />
                     ) : null}
                   </div>
                 </div>
               </div>
             </div>
-
             <Divider />
             {/* </CardContent>
           </Card> */}
@@ -123,96 +105,4 @@ export default function CustomizedTimeline() {
       )}
     </>
   );
-
-  // return (
-  //     {activity.length === 0 && <div>loading</div>}
-  //     {activity.length > 0 && (
-  //       <Timeline position="alternate">
-  //         {activity.sort((a, b) => {
-  //           if (a.createdAt < b.createdAt) {
-  //             return 1;
-  //           } if (a.createdAt > b.createdAt) {
-  //             return -1;
-  //           }
-  //           return 0;
-  //         }).map((data: Activity) => (
-  //           <TimelineItem key={data.createdAt}>
-  //             <TimelineOppositeContent
-  //               sx={{ m: 'auto 0' }}
-  //               align="right"
-  //               variant="body2"
-  //               color="text.secondary"
-  //             >
-  //               {moment(data.createdAt).calendar()}
-  //             </TimelineOppositeContent>
-  //             <TimelineSeparator>
-  //               <TimelineConnector />
-  //               {data.type.toLowerCase() === 'wishlist' ? (
-  //                 <IconButton
-  //                   aria-label="Like minimal photography"
-  //                   size="md"
-  //                   variant="solid"
-  //                   color="danger"
-  //                   sx={{
-  //                   }}
-  //                 >
-  //                   <BookmarkAddIcon />
-  //                 </IconButton>
-  //               ) : data.type.toLowerCase() === 'review' ? (
-  //                 <IconButton
-  //                   aria-label="Like minimal photography"
-  //                   size="md"
-  //                   variant="solid"
-  //                   color="primary"
-  //                   sx={{
-  //                   }}
-  //                 >
-  //                   <StarIcon />
-  //                 </IconButton>
-  //               )
-  //                 : data.type.toLowerCase() === 'location' ? (
-  //                   <IconButton
-  //                     aria-label="Like minimal photography"
-  //                     size="md"
-  //                     variant="solid"
-  //                     color="info"
-  //                     sx={{
-  //                     }}
-  //                   >
-  //                     <AddLocationIcon />
-  //                   </IconButton>
-  //                 ) : (
-  //                   <IconButton
-  //                     aria-label="Like minimal photography"
-  //                     size="md"
-  //                     variant="solid"
-  //                     color="success"
-  //                     sx={{
-  //                     }}
-  //                   >
-  //                     <LocalMallIcon />
-  //                   </IconButton>
-  //                 )}
-  //               <TimelineConnector />
-  //             </TimelineSeparator>
-  //             <TimelineContent sx={{ py: '12px', px: 2 }}>
-  //               <Typography variant="h6" component="span">
-  //                 {data.type.toLowerCase() === 'wishlist' ? (`${data.user.firstName} added ${data.book.title} to their wishlist`)
-  //                   : data.type.toLowerCase() === 'review' ? (
-  //                     (`${data.user.firstName} rated ${data.book.title} ${data.description} stars`)
-  //                   )
-  //                     : data.type.toLowerCase() === 'location' ? (
-  //                       (`${data.user.firstName} added ${data.description} as a reading spot`)
-  //                     )
-  //                       : (
-  //                         `${data.user.firstName} added ${data.book.title} to their owned books`
-  //                       )}
-  //               </Typography>
-  //             </TimelineContent>
-  //           </TimelineItem>
-  //         ))}
-  //       </Timeline>
-  //     )}
-
-  // );
 }
