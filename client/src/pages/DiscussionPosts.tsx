ef872a1f5180552d4bc4e321f186b2e8aea6ec85
kevin <kklein166@gmail.com>
2023-05-29 14:00:16 -0500

    work on posts bug - deployment

--- client/src/pages/DiscussionPosts.tsx
+++ client/src/pages/DiscussionPosts.tsx
@@ -112,12 +112,14 @@ function DiscussionPosts() {
   }, false);
 
   async function fetchImage() {
-    const response = await axios.get(`/api/clubs/discussions/${id}`);
-    // console.log(response);
-    if (response.data && response.data.image) {
-      setDiscussionImage(response.data.image);
+    try {
+      const response = await axios.get(`/api/clubs/discussions/${id}`);
+      if (response.data && response.data.image) {
+        setDiscussionImage(response.data.image);
+      }
+    } catch (error) {
+      console.error(error);
     }
-    setBookTitle(response.data.bookTitle);
   }
 
   useEffect(() => {
